544948dc967c018150ceb7bd99f3706a521349b5
lalkrisfford222-lgtm <lalkrisfford222@gmail.com>
2026-08-19 21:00:35 +0800

    Add files via upload

---
+++ JAVA/DAY 1 - 365 PROGRESS/Day_2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        double time = 7.2;
+        char grades = 'A';
+        boolean isNight = true;
+
+        System.out.println(age);
+        System.out.println(time);
+        System.out.println(grades);
+        System.out.println(isNight);
+    }
+}
