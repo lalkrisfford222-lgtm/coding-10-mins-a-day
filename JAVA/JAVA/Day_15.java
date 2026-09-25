661d771d918df65b1c7e1e6a16ec7a6e19fdd34e
lalkrisfford222-lgtm <lalkrisfford222@gmail.com>
2026-09-25 14:43:52 +0800

    Add files via upload

---
+++ JAVA/JAVA/Day_15.java
@@ -0,0 +1,14 @@
+public class Day_15 {
+    public static void main(String[] args) {
+        //DAY 15 NESTED LOOPS
+
+        for(int i=0; i<5; i++){
+
+            for(int j=0; j<5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
